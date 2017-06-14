49f1a01a94dde86cd8d60bdb588fafff9f464c5b
pulkitgpt27 <pulkitgpt27@gmail.com>
2017-06-14 21:11:25 +0530

    same

--- app/src/main/java/com/example/android/quit_it/NewEntryFragment.java
+++ app/src/main/java/com/example/android/quit_it/NewEntryFragment.java
@@ -34,76 +34,10 @@ public class NewEntryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView=inflater.inflate(R.layout.new_entry,container,false);
-        medical_spinner=(Spinner) rootView.findViewById(R.id.spinner_medical);
-        hobby_spinner=(Spinner)rootView.findViewById(R.id.spinner_Hobby);
-        medical_history="NONE";
-        setupSpinnerMedical();
-        setupSpinnerHobby();
+        View rootView = inflater.inflate(R.layout.new_entry, container, false);
+
         return rootView;
     }
-
-    private void setupSpinnerHobby() {
-        ArrayAdapter hobbySpinnerAdapter=ArrayAdapter.createFromResource(getActivity(),R.array.array_hobby_options, android.R.layout.simple_spinner_item);
-
-        hobbySpinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-
-        hobby_spinner.setAdapter(hobbySpinnerAdapter);
-        hobby_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selection=(String) parent.getItemAtPosition(position);
-                if (!TextUtils.isEmpty(selection)) {
-                    if(selection.equals(getString(R.string.hobby_news))){
-                        hobby="News";
-                    }else if(selection.equals(getString(R.string.hobby_cricket))){
-                        hobby="Cricket";
-                    }else {
-                        hobby="Football";
-                    }
-
-                }
+}
 
 
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                hobby="News";
-            }
-        });
-
-    }
-
-    private void setupSpinnerMedical() {
-        ArrayAdapter medicalSpinnerAdapter=ArrayAdapter.createFromResource(getActivity(),R.array.array_medical_options, android.R.layout.simple_spinner_item);
-
-        medicalSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-
-        medical_spinner.setAdapter(medicalSpinnerAdapter);
-        medical_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selection=(String) parent.getItemAtPosition(position);
-                if (!TextUtils.isEmpty(selection)) {
-                    if(selection.equals(getString(R.string.medical_cancer))){
-                        medical_history="Cancer";
-                    }else if(selection.equals(getString(R.string.medical_Sugar))){
-                        medical_history="Sugar";
-                    }else {
-                        medical_history="Lung_Problem";
-                    }
-
-                }
-
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                medical_history="NONE";
-            }
-        });
-
-    }
-    }
